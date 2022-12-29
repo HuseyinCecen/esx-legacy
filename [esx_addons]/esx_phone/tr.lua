54299985271188dd27f94ae724b6b9aae329bded
R1chter <102230398+HuseyinCecen@users.noreply.github.com>
2022-12-30 01:28:46 +0300

    Create tr.lua
    ESX Phone Turkish Translate

---
+++ [esx_addons]/esx_phone/tr.lua
@@ -0,0 +1,13 @@
+Locales['tr'] = {
+    ['new_message'] = 'Bir mesaj aldınız:e: %s',
+    ['press_take_call'] = '%s - Aramayı cevaplamak için [E] tuşuna basın',
+    ['taken_call'] = '%s çağrıyı kabul etti',
+    ['gps_position'] = 'hedef gps\'inize eklendi',
+    ['message_sent'] = 'mesaj gönderildi',
+    ['cannot_add_self'] = 'kendini ekleyemezsin!',
+    ['number_in_contacts'] = 'bu numara zaten kişi listenizde',
+    ['contact_added'] = 'kişi eklendi!',
+    ['contact_removed'] = 'kişi kaldırıldı!',
+    ['number_not_assigned'] = 'numara atanmadı!',
+    ['invalid_number'] = 'bu geçerli bir sayı değil!',
+  }
